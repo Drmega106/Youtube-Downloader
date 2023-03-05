2dd0161ea6bd636f01a3d25d2410e8a32c0ce676
Drmega106 <125157709+Drmega106@users.noreply.github.com>
2023-03-06 01:50:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ let container = document.querySelector(".container");
 let input = document.querySelector(".vinput input");
 let btn = document.querySelector(".btn");
 let videos = document.querySelector(".videos");
+let vlink = "";
+
+
 
 if (input.value === "") {
     videos.innerHTML = "<span> ...your video will appear here </span>"
@@ -10,39 +13,43 @@ btn.onclick = function () {
     if (input.value === "") {
         videos.innerHTML = "<span> Please type the URL ...</span>"
     }else {
-    let mylink = {
-        "url" : input.value
-       };
-    let xhttp = new XMLHttpRequest;
-    xhttp.open("post", "https://save-from.net/api/convert");
-    xhttp.setRequestHeader("content-type", "application/json");
-    xhttp.send(JSON.stringify(mylink));
-
-    let data = "";
-    let dataobj;
-    xhttp.onload = function () {
-        if (xhttp.status != 200) {
-            videos.innerHTML = "<span> ....incorrect Url ! </span>"
+        if(input.checkValidity()) {
+            let mylink = {
+                "url" : input.value
+               };
+            let xhttp = new XMLHttpRequest;
+            xhttp.open("post", "https://save-from.net/api/convert");
+            xhttp.setRequestHeader("content-type", "application/json");
+            xhttp.send(JSON.stringify(mylink));
+        
+            let data = "";
+            let dataobj;
+            xhttp.onload = function () {
+                if (xhttp.status != 200) {
+                    videos.innerHTML = "<span> ....incorrect Url ! </span>"
+                } else {
+                    videos.innerHTML = "";
+                    data = this.responseText;
+                    dataobj = JSON.parse(data);
+                    let video = document.createElement("div");
+                    video.classList.add("video");
+                    video.innerHTML =  ` <img src=${dataobj.thumb} alt="thumb" class="vimg"</img>  
+                    <div class="vname">${dataobj.meta.title}</div>` ;
+                    videos.appendChild(video);
+                    let urls = dataobj.url.slice(0,2);
+                    urls.forEach( e => {
+                        let downloadbtn = document.createElement("a");
+                        downloadbtn.classList.add("downloadbtn");
+                        downloadbtn.innerHTML= `${e.subname} ${e.type} ( size: ${e.filesize? Math.round(e.filesize/1024/1024)+" Mb" : "unknown"})`;
+                        downloadbtn.href = e.url;
+                        downloadbtn.target = "_blank";
+                        videos.appendChild(downloadbtn);
+                    });
+        };
+        };
         } else {
-            videos.innerHTML = "";
-            data = this.responseText;
-            dataobj = JSON.parse(data);
-            let video = document.createElement("div");
-            video.classList.add("video");
-            video.innerHTML =  ` <img src=${dataobj.thumb} alt="thumb" class="vimg"</img>  
-            <div class="vname">${dataobj.meta.title}</div>` ;
-            videos.appendChild(video);
-            let urls = dataobj.url.slice(0,2);
-            urls.forEach( e => {
-                let downloadbtn = document.createElement("a");
-                downloadbtn.classList.add("downloadbtn");
-                downloadbtn.innerHTML= `${e.subname} ${e.type}`;
-                downloadbtn.href = e.url;
-                downloadbtn.target = "_blank";
-                videos.appendChild(downloadbtn);
+            videos.innerHTML = input.validationMessage;
+        }
 
-            });
-};
-};
 };
 };
